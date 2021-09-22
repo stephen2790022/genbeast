12d126912a4b190fc722120e20a5425ce12d5caa
stephen2790022 <toulouse.stephen1.com>
2021-09-22 17:40:59 +0900

    glitch fixed

--- src/components/Glitch/index.jsx
+++ src/components/Glitch/index.jsx
@@ -6,7 +6,7 @@ const Glitch = () => {
     return (
         <div id="app">
             <div id="wrapper">
-                <h1 className="glitch" data-text="Discover Collect And Sell NFT Assets">Discover Collect And Sell NFT Assets</h1>
+                <h1 className="glitch" data-text="Discover Collect And Sell NFT Assets">Discover Collect And  Sell NFT Assets</h1>
                 <span className="sub"></span>
             </div>
         </div>
