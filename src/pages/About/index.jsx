d2e78a26406836cf7b0ef1547d45d96562e8563c
stephen2790022 <toulouse.stephen1.com>
2021-10-30 22:30:45 +0900

    add coming soon page to unfinished one

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ComingSoon from "../../components/ComingSoon";
 const About = () => {
+    React.useEffect(() => {
+        window.scrollTo(0, 0)
+    },[])
     return (
         <ComingSoon/>
     );
